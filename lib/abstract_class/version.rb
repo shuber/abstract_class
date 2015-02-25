3b6085e45a0be77e530b032ef043683b9a6ad5bc
Sean Huber <sean@onmyblock.com>
2015-02-24 18:05:37 -0800

    Shorten lines in AbstractClass::Version

--- lib/abstract_class/version.rb
+++ lib/abstract_class/version.rb
@@ -5,7 +5,9 @@ module AbstractClass
     MINOR = 0
     PATCH = 2
 
-    # Returns a version string by joining <tt>MAJOR</tt>, <tt>MINOR</tt>, and <tt>PATCH</tt> with <tt>'.'</tt>
+    # Returns a version string by joining
+    # <tt>MAJOR</tt>, <tt>MINOR</tt>, and <tt>PATCH</tt>
+    # with <tt>'.'</tt>
     #
     # Example
     #
