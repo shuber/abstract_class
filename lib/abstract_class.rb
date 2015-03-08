e53ce011c1a356559c28b0029fd5085e666e0caf
Sean Huber <github@shuber.io>
2015-03-08 11:07:00 -0700

    Don't auto include `AbstractClass` in `Class`
    Explicitly add this behavior to your classes by `extend AbstractClass`

--- lib/abstract_class.rb
+++ lib/abstract_class.rb
@@ -1,52 +1,36 @@
 require 'abstract_class/version'
 
-# Adds the ability to declare classes as abstract
-# so that they can't be instantiated or allocated
+# Declares a class as abstract which prevents instantiation or allocation.
 module AbstractClass
+  # The exception raised when an abstract class is instantiated or allocated.
   Error = Class.new(RuntimeError)
 
-  # When included, it will <tt>alias_method_chain</tt> the <tt>allocate</tt>
-  # and <tt>new</tt> methods with <tt>:abstract_class</tt> in <tt>klass</tt>
-  def self.included(klass)
-    klass.class_eval do
-      alias_method :allocate_without_abstract_class, :allocate
-      alias_method :allocate, :allocate_with_abstract_class
-
-      alias_method :new_without_abstract_class, :new
-      alias_method :new, :new_with_abstract_class
-    end
+  def self.extended(mod)
+    mod.abstract_classes << mod
   end
 
-  # Declares this class as abstract and prevents
-  # it from being instantiated or allocated
-  def abstract
-    @abstract = true
+  # Stores a collection of classes that have been declared as abstract.
+  def abstract_classes
+    @@abstract_classes ||= []
   end
 
-  # Checks if this class has been declared as abstract
-  def abstract?
-    @abstract ||= false
+  # Raises {AbstractClass::Error} if the class is abstract
+  def allocate(*args)
+    raise_if_abstract(:allocated) { super }
   end
 
-  # Raises RuntimeError if class is abstract, otherwise
-  # returns <tt>allocate_without_abstract_class</tt>
-  def allocate_with_abstract_class
-    if abstract?
-      raise Error, "abstract class #{self} can't be allocated"
+  # Raises {AbstractClass::Error} if the class is abstract
+  def new(*args)
+    raise_if_abstract(:instantiated) { super }
+  end
+
+  private
+
+  def raise_if_abstract(type)
+    if abstract_classes.include?(self)
+      raise Error, "abstract class #{self} can't be #{type}"
     else
-      allocate_without_abstract_class
-    end
-  end
-
-  # Raises RuntimeError if class is abstract, otherwise
-  # returns <tt>new_without_abstract_class</tt>
-  def new_with_abstract_class(*args, &block)
-    if abstract?
-      raise Error, "abstract class #{self} can't be instantiated"
-    else
-      new_without_abstract_class(*args, &block)
+      yield
     end
   end
 end
-
-Class.send(:include, AbstractClass)
--- spec/abstract_class_spec.rb
+++ spec/abstract_class_spec.rb
@@ -1,51 +1,41 @@
 require_relative '../lib/abstract_class'
 
 RSpec.describe AbstractClass do
-  let(:abstract) { Class.new { abstract } }
-  let(:derived)  { Class.new abstract }
+  let(:abstract) { Class.new.extend(described_class) }
+  let(:derived)  { Class.new(abstract) }
   let(:error)    { described_class::Error }
 
-  describe '.abstract?' do
-    it 'should return true if the class is abstract' do
-      expect(abstract).to be_abstract
+  describe '.abstract_classes' do
+    it 'should include the abstract class' do
+      expect(abstract.abstract_classes).to include(abstract)
     end
 
-    it 'should return false if the class is not abstract' do
-      expect(derived).not_to be_abstract
+    it 'should only include abstract derived classes' do
+      expect(abstract.abstract_classes).not_to include(derived)
+      derived.extend(described_class)
+      expect(abstract.abstract_classes).to include(derived)
     end
   end
 
   describe '.allocate' do
     it 'should not allow an abstract class to be allocated' do
       initializer = -> { abstract.allocate }
-      expect(initializer).to raise_error error
+      expect(initializer).to raise_error(error)
     end
 
     it 'should allow a derived class to be allocated' do
-      initializer = -> { derived.allocate }
-      expect(initializer).not_to raise_error
-    end
-
-    it 'should allow an abstract class to bypass restrictions' do
-      initializer = -> { derived.allocate_without_abstract_class }
-      expect(initializer).not_to raise_error
+      expect(derived.allocate).to be_is_a(derived)
     end
   end
 
   describe '.new' do
     it 'should not allow an abstract class to be initialized' do
       initializer = -> { abstract.new }
-      expect(initializer).to raise_error error
+      expect(initializer).to raise_error(error)
     end
 
     it 'should allow a derived class to be initialized' do
-      initializer = -> { derived.new }
-      expect(initializer).not_to raise_error
-    end
-
-    it 'should allow an abstract class to bypass restrictions' do
-      initializer = -> { derived.new_without_abstract_class }
-      expect(initializer).not_to raise_error
+      expect(derived.new).to be_is_a(derived)
     end
   end
 end
